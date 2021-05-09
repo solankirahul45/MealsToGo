9c2e8ec3899c6e18d223f676e2ff096ad2bd3677
Rahul Solanki <solanki.rahul45@gmail.com>
2021-05-09 20:58:53 +0530

    Spacer components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -7,6 +7,7 @@ import SvgXml from 'react-native-svg';
 
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const RestaurantCard = styled(Card)`
     background-color: ${(props) => props.theme.colors.bg.primary};
@@ -84,13 +85,15 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                             )
                         }
 
-                        <View style={{ paddingLeft: 16 }} />
-                        {
-                            isOpenNow && <SvgXml xml={open} width={20} height={20} />                            
-                        }
-
-                        <View style={{ paddingLeft: 16 }} />
-                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+                        <Spacer position="left" size="large">
+                            {
+                                isOpenNow && <SvgXml xml={open} width={20} height={20} />                            
+                            }
+                        </Spacer>                            
+                        
+                        <Spacer position="left" size="large">
+                            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+                        </Spacer>                        
                     </SectionEnd>
                 </Section>
                 <Address>{address}</Address>
